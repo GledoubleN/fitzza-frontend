b333e165a5a6c6eb92a6173f7d8c7efea0abf9f7
Junwoo Shin <wnsdn020707@gmail.com>
2026-09-17 17:07:22 +0900

    feat: 회원가입 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { MainPage } from "./pages/MainPage.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ShoppingCartPage } from "./pages/ShoppingCartPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <MainPage/> }/>
-        <Route path="/shopping-cart" element={ <ShoppingCartPage/> }/>
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <MainPage/> }/>
+                <Route path="/shopping-cart" element={ <ShoppingCartPage/> }/>
+                <Route path="/signup" element={ <SignupPage/> }/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App
+export default App;
