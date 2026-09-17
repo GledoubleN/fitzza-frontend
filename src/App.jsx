5fa94a4c76a92271bf2e5f20f43da32a88e4bf4b
fractalbarista <fractalbarista@gmail.com>
2026-09-17 16:13:36 +0900

    BrowserRouter 컴포넌트를 사용하도록 수정 component 프롭스 대신에 element 프롭스를 사용하도록 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import { MainPage } from "./pages/MainPage.jsx";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ShoppingCartPage } from "./pages/ShoppingCartPage.jsx";
 
 function App() {
   return (
-    <Browser>
-     <Routes>
-       <Route path="/" component={MainPage} />
-       <Route path="/shopping-cart" component={ShoppingCartPage} />
-     </Routes>
-    </Browser>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <MainPage/> }/>
+        <Route path="/shopping-cart" element={ <ShoppingCartPage/> }/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
