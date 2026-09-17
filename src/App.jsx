0b1a2a13473c1810715cf7d497e74004cea48ebb
fractalbarista <fractalbarista@gmail.com>
2026-09-17 15:12:08 +0900

    react-router-dom 설치

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import { MainPage } from "./pages/MainPage.jsx";
+import { Route, Routes } from "react-router-dom";
+import { ShoppingCartPage } from "./pages/ShoppingCartPage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <MainPage></MainPage>
+    <Browser>
+     <Routes>
+       <Route path="/" component={MainPage} />
+       <Route path="/shopping-cart" component={ShoppingCartPage} />
+     </Routes>
+    </Browser>
   )
 }
 
