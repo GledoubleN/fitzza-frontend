0ea21959e0102faac80c069c85cb07e774845d1f
Glenn <glenndev7606@gmail.com>
2026-09-17 17:02:45 +0900

    SignInPage 추가 및 라우트 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { MainPage } from "./pages/MainPage.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ShoppingCartPage } from "./pages/ShoppingCartPage.jsx";
+import {SignInPage} from "./pages/SignInPage.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <MainPage/> }/>
         <Route path="/shopping-cart" element={ <ShoppingCartPage/> }/>
+        <Route path="/signInPage" element={ <SignInPage/> }/>
       </Routes>
     </BrowserRouter>
   )
