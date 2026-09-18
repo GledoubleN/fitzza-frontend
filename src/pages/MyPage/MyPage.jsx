9285b5e7f39c7217deb593b89dbe1ebaa2e79290
Junwoo Shin <wnsdn020707@gmail.com>
2026-09-18 14:04:06 +0900

    feat: add mypage header

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,9 +1,34 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Heading, Button } from '@chakra-ui/react';
 import './MyPage.css';
 
 export default function MyPage() {
     return (
         <Box className="my-page">
+
+            <Flex
+                className="my-header"
+                align="center"
+                justify="space-between"
+                px="20px"
+                height="60px"
+                borderBottom="1px solid"
+                borderColor="gray.100"
+            >
+                <Heading
+                    size="md"
+                    fontWeight="700"
+                >
+                    마이페이지
+                </Heading>
+
+                <Button
+                    variant="ghost"
+                    size="sm"
+                >
+                    고객센터
+                </Button>
+            </Flex>
+
         </Box>
     );
 }
