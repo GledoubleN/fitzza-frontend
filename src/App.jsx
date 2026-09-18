2f8d91b920a3f045bdba3ce796de677d32bf95b8
Junwoo Shin <wnsdn020707@gmail.com>
2026-09-18 14:01:26 +0900

    feat: add mypage base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { MainPage } from "./pages/MainPage.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ShoppingCartPage } from "./pages/ShoppingCartPage.jsx";
-import SignupPage from "./pages/SignupPage.jsx";
+import MyPage from './pages/MyPage/MyPage';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={ <MainPage/> }/>
-                <Route path="/shopping-cart" element={ <ShoppingCartPage/> }/>
-                <Route path="/signup" element={ <SignupPage/> }/>
-            </Routes>
-        </BrowserRouter>
-    )
+        <MyPage />
+    );
 }
 
 export default App;
---
+++ src/pages/MyPage/MyPage.jsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react';
+import './MyPage.css';
+
+export default function MyPage() {
+    return (
+        <Box className="my-page">
+        </Box>
+    );
+}
