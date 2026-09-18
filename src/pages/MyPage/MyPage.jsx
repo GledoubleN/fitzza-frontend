19ca938cec1919ef9fe88f5005f2999fc7c9d0fc
Junwoo Shin <wnsdn020707@gmail.com>
2026-09-18 14:12:25 +0900

    feat: add mypage summary

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -56,7 +56,59 @@ export default function MyPage() {
                     @fit_user
                 </Text>
             </Flex>
+            <Box
+                className="summary-section"
+                mx="16px"
+                px="10px"
+                py="18px"
+                border="1px solid"
+                borderColor="gray.100"
+                borderRadius="16px"
+            >
+                <Flex align="center">
 
+                    <Box flex="1" px="12px">
+                        <Text
+                            fontSize="13px"
+                            color="gray.500"
+                            mb="4px"
+                        >
+                            적립금
+                        </Text>
+
+                        <Text
+                            fontSize="17px"
+                            fontWeight="600"
+                        >
+                            7,777원
+                        </Text>
+                    </Box>
+
+                    <Box
+                        width="1px"
+                        height="35px"
+                        bg="gray.100"
+                    />
+
+                    <Box flex="1" px="12px">
+                        <Text
+                            fontSize="13px"
+                            color="gray.500"
+                            mb="4px"
+                        >
+                            쿠폰
+                        </Text>
+
+                        <Text
+                            fontSize="17px"
+                            fontWeight="600"
+                        >
+                            11장
+                        </Text>
+                    </Box>
+
+                </Flex>
+            </Box>
         </Box>
     );
 }
