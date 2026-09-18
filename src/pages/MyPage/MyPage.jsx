aa71656022923890f39cb7037b5385cc2d676277
Junwoo Shin <wnsdn020707@gmail.com>
2026-09-18 14:23:16 +0900

    feat: add mypage snap section

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -222,6 +222,52 @@ export default function MyPage() {
                         </Text>
                     </Flex>
                 </Flex>
+                <Box
+                    className="snap-section"
+                    px="16px"
+                    pt="20px"
+                >
+                    <Flex
+                        justify="space-between"
+                        align="center"
+                        mb="12px"
+                    >
+                        <Heading
+                            size="sm"
+                            fontWeight="700"
+                        >
+                            나의 스냅
+                        </Heading>
+
+                        <Button
+                            variant="ghost"
+                            size="sm"
+                            color="gray.500"
+                        >
+                            전체보기 &gt;
+                        </Button>
+                    </Flex>
+
+                    <Box className="snap-grid">
+
+                        <Box className="snap-item">
+                            <img src="/images/snap1.jpg" alt="나의 스냅 1" />
+                        </Box>
+
+                        <Box className="snap-item">
+                            <img src="/images/snap2.jpg" alt="나의 스냅 2" />
+                        </Box>
+
+                        <Box className="snap-item">
+                            <img src="/images/snap3.jpg" alt="나의 스냅 3" />
+                        </Box>
+
+                        <Box className="snap-item">
+                            <img src="/images/snap4.jpg" alt="나의 스냅 4" />
+                        </Box>
+
+                    </Box>
+                </Box>
             </Box>
         </Box>
     );
