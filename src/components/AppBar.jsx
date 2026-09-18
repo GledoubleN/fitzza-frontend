4a68169d5f494ca23ad6d3d0b68f547775bb30f0
fractalbarista <fractalbarista@gmail.com>
2026-09-18 13:17:14 +0900

    장바구니 버튼을 누르면 ShoppingCartPage로 이동하도록 수정

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,7 +1,10 @@
 import { Grid, GridItem, IconButton, Stack, Text } from '@chakra-ui/react'
 import { LuBell, LuShoppingBag } from 'react-icons/lu'
+import { useNavigate } from 'react-router-dom'
 
 export const AppBar = () => {
+  const navigate = useNavigate();
+
   return (
     <Grid templateColumns={ 'auto 1fr auto auto' } gap={ '4' }>
       <GridItem alignContent={ 'center' }>
@@ -14,7 +17,7 @@ export const AppBar = () => {
         </IconButton>
       </GridItem>
       <GridItem>
-        <IconButton rounded={ 'full' }>
+        <IconButton rounded={ 'full' } onClick={() => { navigate('/shopping-cart') }}>
           <LuShoppingBag></LuShoppingBag>
         </IconButton>
       </GridItem>
