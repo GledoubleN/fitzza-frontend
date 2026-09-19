fd40b653fdddfd362ccbc0e5426f32260f27d274
fractalbarista <fractalbarista@gmail.com>
2026-09-19 15:59:00 +0900

    RichTextEditor 컴포넌트 추가 관련 패키지 설치

--- src/pages/community/CommunityWritePage.jsx
+++ src/pages/community/CommunityWritePage.jsx
@@ -1,6 +1,39 @@
+import { Button, Container, Stack } from '@chakra-ui/react'
+import { AppBar } from '../../components/AppBar.jsx'
+import { Control, RichTextEditor } from '/src/components/ui/rich-text-editor'
+import { useEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import { useState } from 'react'
+
 export const CommunityWritePage = () => {
+  const [editable, setEditable] = useState(true)
+
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: `<p>Edit this text...</p>`,
+    editable,
+    shouldRerenderOnTransaction: true,
+    immediatelyRender: false,
+  })
 
   return (
-    <p>This is Write Page.</p>
+    <Container maxWidth={ 'xl' } height={ '100vh' }>
+      <Stack paddingY={ '4' } gap={ '4' }>
+        <AppBar></AppBar>
+
+        <RichTextEditor.Root editor={ editor } height={ '400px' }>
+          <RichTextEditor.Toolbar>
+            <RichTextEditor.ControlGroup>
+              <Control.Bold/>
+              <Control.Italic/>
+              <Control.Underline/>
+            </RichTextEditor.ControlGroup>
+          </RichTextEditor.Toolbar>
+          <RichTextEditor.Content/>
+        </RichTextEditor.Root>
+
+        <Button>작성하기</Button>
+      </Stack>
+    </Container>
   )
 }
