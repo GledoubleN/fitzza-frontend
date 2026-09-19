c4505a45cf06a131ef16e09dc59959fe84f0eb28
Junwoo Shin <wnsdn020707@gmail.com>
2026-09-19 10:40:39 +0900

    refactor: extract mypage follow component

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -23,60 +23,8 @@ export default function MyPage() {
             <MySummary />
 
             <MyMenu />
-                <Flex
-                    className="follow-section"
-                    justify="center"
-                    align="center"
-                    py="16px"
-                >
-                    <Flex
-                        flex="1"
-                        direction="column"
-                        align="center"
-                    >
-                        <Text
-                            fontSize="13px"
-                            color="gray.500"
-                        >
-                            팔로잉
-                        </Text>
 
-                        <Text
-                            fontSize="16px"
-                            fontWeight="600"
-                            mt="3px"
-                        >
-                            36
-                        </Text>
-                    </Flex>
-
-                    <Box
-                        width="1px"
-                        height="28px"
-                        bg="gray.100"
-                    />
-
-                    <Flex
-                        flex="1"
-                        direction="column"
-                        align="center"
-                    >
-                        <Text
-                            fontSize="13px"
-                            color="gray.500"
-                        >
-                            팔로워
-                        </Text>
-
-                        <Text
-                            fontSize="16px"
-                            fontWeight="600"
-                            mt="3px"
-                        >
-                            128
-                        </Text>
-                    </Flex>
-                </Flex>
+            <FollowInfo />
                 <Box
                     className="snap-section"
                     px="16px"
