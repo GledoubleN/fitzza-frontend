06783700e38c41dc3433b10673f6ac499fe5fc7a
fractalbarista <fractalbarista@gmail.com>
2026-09-21 10:32:15 +0900

    비교와 투표를 입력할 수 있는 UI 추가

--- src/pages/community/CommunityWritePage.jsx
+++ src/pages/community/CommunityWritePage.jsx
@@ -1,12 +1,26 @@
-import { Button, Container, Stack } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  Container,
+  Grid,
+  Group,
+  IconButton,
+  Input, InputGroup,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
 import { AppBar } from '../../components/AppBar.jsx'
 import { Control, RichTextEditor } from '/src/components/ui/rich-text-editor'
 import { useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { useState } from 'react'
+import { LuCircle, LuPlus, LuTrash } from 'react-icons/lu'
 
 export const CommunityWritePage = () => {
   const [editable, setEditable] = useState(true)
+  const [mode, setMode] = useState('default')
 
   const editor = useEditor({
     extensions: [StarterKit],
@@ -16,10 +30,72 @@ export const CommunityWritePage = () => {
     immediatelyRender: false,
   })
 
+  function getAdditionalInput () {
+    switch (mode) {
+      case 'default':
+        return (
+          <></>
+        )
+
+      case 'versus':
+        return (
+          <Grid templateColumns={ '1fr auto 1fr' } gap={ 4 }>
+            <Card.Root>
+              <Card.Body>
+                <IconButton variant={ 'ghost' }>
+                  <LuPlus></LuPlus>
+                </IconButton>
+              </Card.Body>
+            </Card.Root>
+            <Text alignContent={ 'center' }>VS</Text>
+            <Card.Root>
+              <Card.Body>
+                <IconButton variant={ 'ghost' }>
+                  <LuPlus></LuPlus>
+                </IconButton>
+              </Card.Body>
+            </Card.Root>
+          </Grid>
+        )
+
+      case 'vote':
+        return (
+          <Stack direction={ 'column' } gap={ '2' }>
+            <Group attached>
+              <Input></Input>
+              <IconButton variant={ 'outline' }>
+                <LuTrash></LuTrash>
+              </IconButton>
+            </Group>
+            <Group attached>
+              <Input></Input>
+              <IconButton variant={ 'outline' }>
+                <LuTrash></LuTrash>
+              </IconButton>
+            </Group>
+            <Button>추가하기</Button>
+          </Stack>
+        )
+
+      default:
+        throw Error
+    }
+  }
+
   return (
     <Container maxWidth={ 'xl' } height={ '100vh' }>
       <Stack paddingY={ '4' } gap={ '4' }>
         <AppBar></AppBar>
+
+        <Grid templateColumns={ 'repeat(3, 1fr)' } gap={ '4' }>
+          <Button onClick={ () => {setMode('default')} }>기본</Button>
+          <Button onClick={ () => {setMode('versus')} }>비교</Button>
+          <Button onClick={ () => {setMode('vote')} }>투표</Button>
+        </Grid>
+
+        {
+          getAdditionalInput()
+        }
 
         <RichTextEditor.Root editor={ editor } height={ '400px' }>
           <RichTextEditor.Toolbar>
